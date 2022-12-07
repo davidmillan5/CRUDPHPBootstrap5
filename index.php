4191dca46953075101d93eacd75209c4db9e9da7
Cesar David Millan <cesardavidmillan5@gmail.com>
2022-12-06 23:00:09 -0500

    Finalize CRUD with PHP, MySQL and Bootstrap5

--- index.php
+++ index.php
@@ -1,16 +1,94 @@
-<?php include 'template/header.php'?>
+<?php include 'template/header.php' ?>
 
+<?php
+include_once "model/conexion.php";
+$sentencia = $bd->query("select * from persona");
+$persona = $sentencia->fetchAll(PDO::FETCH_OBJ);
+//print_r($persona);
+
+
+?>
 
 <div class="container mt-5">
   <div class="row justify-content-center">
     <div class="col-md-7">
+      <!--Inicio alerta -->
+      <?php
+      if (isset($_GET['mensaje']) and $_GET['mensaje'] == 'falta') {
+
+      ?>
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+          <strong>Error!</strong> Rellena todos los campos.
+          <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+      <?php
+      }
+      ?>
+
+
+      <?php
+      if (isset($_GET['mensaje']) and $_GET['mensaje'] == 'registrado') {
+
+      ?>
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+          <strong>Registrado!</strong> Se agregaron los datos.
+          <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+      <?php
+      }
+      ?>
+
+
+      <?php
+      if (isset($_GET['mensaje']) and $_GET['mensaje'] == 'error') {
+
+      ?>
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+          <strong>Error!</strong> Vuelve a intentar.
+          <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+      <?php
+      }
+      ?>
+
+
+
+      <?php
+      if (isset($_GET['mensaje']) and $_GET['mensaje'] == 'editado') {
+
+      ?>
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+          <strong>Cambiado!</strong> Los datos fueron actualizados.
+          <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+      <?php
+      }
+      ?>
+
+
+      <?php
+      if (isset($_GET['mensaje']) and $_GET['mensaje'] == 'eliminado') {
+
+      ?>
+        <div class="alert alert-warning alert-dismissible fade show" role="alert">
+          <strong>Eliminado!</strong> Los datos fueron borrados.
+          <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+      <?php
+      }
+      ?>
+
+
+
+
+      <!-- Fin Alerta -->
       <div class="card">
         <div class="card-header">
           Lista de Personas
         </div>
         <div class="p-4">
           <div class="table-responsive">
-            <table class="table table-primary align-middle">
+            <table class="table table-Light align-middle">
               <thead>
                 <tr>
                   <th scope="col">#</th>
@@ -21,27 +99,55 @@
                 </tr>
               </thead>
               <tbody>
-                <tr class="">
-                  <td scope="row">1</td>
-                  <td>Cesar Millan</td>
-                  <td>33</td>
-                  <td>Geminis</td>
-                  <td>Editar</td>
-                  <td>Eliminar</td>
-                </tr>
+                <?php
+                foreach ($persona as $dato) {
+                ?>
+                  <tr class="">
+                    <td scope="row"><?php echo $dato->codigo; ?></td>
+                    <td><?php echo $dato->nombre; ?></td>
+                    <td><?php echo $dato->edad; ?></td>
+                    <td><?php echo $dato->signo; ?></td>
+                    <td><a href="editar.php?codigo=<?php echo $dato->codigo; ?>"><i class="text-success bi bi-pencil-square"></i></a></td>
+                    <td><a onclick="return confirm('¿Estas seguro de eliminar?');" href=" eliminar.php?codigo=<?php echo $dato->codigo; ?>"><i class="text-danger bi bi-trash3"></i></a></td>
+                  </tr>
+                <?php
+                }
+                ?>
               </tbody>
             </table>
           </div>
-          
+
         </div>
       </div>
     </div>
     <div class="col-md-4">
-      DOS
+      <div class="card">
+        <div class="card-header">
+          Ingresar datos:
+          <form class="p-4" method="POST" action="registrar.php">
+            <div class="mb-3">
+              <label class="form-label">Nombre: </label>
+              <input type="text" class="form-control" name="txtNombre" autofocus required>
+            </div>
+            <div class="mb-3">
+              <label class="form-label">Edad: </label>
+              <input type="number" class="form-control" name="txtEdad" autofocus required>
+            </div>
+            <div class="mb-3">
+              <label class="form-label">Signo: </label>
+              <input type="text" class="form-control" name="txtSigno" autofocus required>
+            </div>
+            <div class="d-grid">
+              <input type="hidden" name="oculto" value="1">
+              <input type="submit" class="btn btn-primary" value="Registrar">
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   </div>
 </div>
 
 
 
-<?php include 'template/footer.php'?>
+<?php include 'template/footer.php' ?>
--- template/header.php
+++ template/header.php
@@ -8,8 +8,14 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
   <!-- Bootstrap CSS v5.2.1 -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
+  <!-- cdn icons-->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
+
+
+  <!-- awesome icons, google icons--->
+
 
 </head>
 
