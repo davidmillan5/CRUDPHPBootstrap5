dc75a53318334690088aaf36dcd3681d4f38381e
Cesar David Millan <cesardavidmillan5@gmail.com>
2022-12-06 21:03:06 -0500

    Initial commit for a CRUD program with PHP and Bootstrap 5

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php include 'template/header.php'?>
+
+
+<div class="container mt-5">
+  <div class="row justify-content-center">
+    <div class="col-md-7">
+      <div class="card">
+        <div class="card-header">
+          Lista de Personas
+        </div>
+        <div class="p-4">
+          <div class="table-responsive">
+            <table class="table table-primary align-middle">
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Nombre</th>
+                  <th scope="col">Edad</th>
+                  <th scope="col">Signo</th>
+                  <th scope="col" colspan="2">Opciones</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr class="">
+                  <td scope="row">1</td>
+                  <td>Cesar Millan</td>
+                  <td>33</td>
+                  <td>Geminis</td>
+                  <td>Editar</td>
+                  <td>Eliminar</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4">
+      DOS
+    </div>
+  </div>
+</div>
+
+
+
+<?php include 'template/footer.php'?>
